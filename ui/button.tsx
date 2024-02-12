79320318bd858a4c7469369a93d6e58a095d39e0
Eric <eric@spiritsdontfly.com>
2024-02-12 17:27:53 +0100

    Refactor button styles

--- ui/button.tsx
+++ ui/button.tsx
@@ -13,11 +13,11 @@ export default function Button({
     <span className="relative inline-flex">
       <button
         className={clsx(
-          'mt-3 inline-flex items-center rounded-md bg-slate-800 px-12 py-2 text-sm font-semibold leading-6 shadow-lg ring-1 ring-slate-200/20 transition duration-150 ease-in-out disabled:cursor-not-allowed disabled:opacity-70',
+          'mt-3 inline-flex items-center rounded-md bg-slate-800 px-12 py-2 text-sm font-semibold leading-6 ring-1 ring-slate-200/20 transition duration-150 ease-in-out disabled:cursor-not-allowed disabled:opacity-70',
           {
-            'text-blue-400 shadow-blue-950/10 hover:text-blue-300 disabled:hover:text-blue-400':
+            'text-blue-400 enabled:hover:text-blue-300 enabled:hover:shadow-[0_0_0.5em_0em_rgba(96,165,250,0.6)]':
               variant === 'default',
-            'text-pink-500 shadow-pink-950/10 hover:text-pink-400 disabled:hover:text-pink-500':
+            'text-pink-500 enabled:hover:text-pink-400 enabled:hover:shadow-[0_0_0.5em_0em_rgba(236,72,153,0.6)]':
               variant === 'pink',
           },
         )}
