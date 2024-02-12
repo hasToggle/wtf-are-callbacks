c199ca08427ab0432083c3511f0ec8016db6600b
Eric <eric@spiritsdontfly.com>
2024-02-12 14:55:46 +0100

    Update color styles for Label and Button components

--- ui/button.tsx
+++ ui/button.tsx
@@ -13,10 +13,12 @@ export default function Button({
     <span className="relative inline-flex">
       <button
         className={clsx(
-          'mt-3 inline-flex items-center rounded-md bg-slate-800 px-12 py-2 text-sm font-semibold leading-6 shadow ring-1 ring-slate-200/20 transition duration-150 ease-in-out disabled:cursor-not-allowed disabled:opacity-70',
+          'mt-3 inline-flex items-center rounded-md bg-slate-800 px-12 py-2 text-sm font-semibold leading-6 shadow-lg ring-1 ring-slate-200/20 transition duration-150 ease-in-out disabled:cursor-not-allowed disabled:opacity-70',
           {
-            'text-blue-400 hover:text-blue-300': variant === 'default',
-            'text-pink-500 hover:text-pink-400': variant === 'pink',
+            'text-blue-400 shadow-blue-950/10 hover:text-blue-300 disabled:hover:text-blue-400':
+              variant === 'default',
+            'text-pink-500 shadow-pink-950/10 hover:text-pink-400 disabled:hover:text-pink-500':
+              variant === 'pink',
           },
         )}
         {...props}
